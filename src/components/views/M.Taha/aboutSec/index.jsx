df6c3e8bd1cdc02df919b4d1aa5a21415061b9a4
Taha Bin Shafiq <muhammadtahaaa7@gmail.com>
2025-10-13 01:50:40 +0500

    complete About Sec UI

--- src/components/views/M.Taha/aboutSec/index.jsx
+++ src/components/views/M.Taha/aboutSec/index.jsx
@@ -1,34 +1,32 @@
 import EditButton from "./editButton"
+import Intro from "./Introduction"
 import Profile from "./profileImage"
 import ProfileName from "./profileName&About"
+import SocialIcons from "./Social-icons"
 function AboutSec() {
+    let profileObj = {
+        fullName: "Taha Bin Shafiq",
+        bio: "Front-End Developer and Learner",
+        socialMedia: [],
+        intro: ""
+    }
     return (
-        <section className="flex h-auto">
+        <section className="flex h-[100vh] justify-center bg-black">
             <aside className="basis-1/3 h-auto flex justify-start pl-10">
                 <div className="flex flex-col justify-start gap-2">
                     <Profile />
                     <div className="flex flex-col gap-3">
-                        <ProfileName />
+                        <ProfileName profileDetails={profileObj} />
                         <EditButton />
                     </div>
-                    <div className="flex flex-col border-1 border-amber-50 h-[200px]">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                    <div>
+                        <SocialIcons />
                     </div>
                 </div>
             </aside>
-            <aside className="basis-2/3 pt-0 pr-20 pl-20">
-                <div>
-                    <p className="text-white">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquam deleniti accusamus amet nihil harum aspernatur quo asperiores explicabo dolores in ea natus, esse sequi architecto libero maiores eligendi vero!
-                        Qui tempore temporibus quidem alias enim quo debitis illum harum, assumenda at. Autem eveniet ea et adipisci voluptate quaerat similique perferendis? Exercitationem autem minima maxime debitis praesentium porro. Doloremque, sed!
-                        Dolor impedit alias voluptate aut repellat porro delectus, obcaecati perspiciatis odit expedita beatae, aliquam assumenda est quia nemo molestiae hic et? Amet, dicta repellendus aperiam soluta at non quo ratione?
-                        Odit laboriosam vel earum, excepturi quae fuga, voluptate iste doloribus possimus animi mollitia optio. Itaque impedit quasi voluptatum nam quos, perferendis labore quidem sequi fuga, aliquid sapiente culpa corporis autem.
-                        Eos ab, et, quae sunt voluptates neque eaque quisquam nam commodi voluptatum nihil. Sint nobis dolorum provident illo odit. Quisquam laboriosam, nam sapiente soluta porro rerum quasi autem minus? Iure?
-                    </p>
+            <aside className="basis-2/3 pt-0 pr-20 pl-0">
+                <div className="border-1 border-amber-50 p-5 rounded-[5px] w-[100%]">
+                    <Intro />
                 </div>
             </aside>
         </section>
--- src/components/views/M.Taha/aboutSec/profileName&About/index.jsx
+++ src/components/views/M.Taha/aboutSec/profileName&About/index.jsx
@@ -1,11 +1,10 @@
-function ProfileName(){
-    return(
+function ProfileName(profileObj) {
+    let { fullName, bio } = profileObj.profileDetails
+    return (
         <>
-        <h2 className="text-white text-3xl pl-0 text-left font-medium">Taha Bin Shfaiq</h2>
-        <p className="text-white text-left">Front-End Developer and Learner</p>
+            <h2 className="text-white text-3xl pl-0 text-left font-medium">{fullName}</h2>
+            <p className="text-white text-left">{bio}</p>
         </>
-        
     )
 }
-
 export default ProfileName
